885fba678e72ecd50d7128e30c2067a8b0f67508
Abdulrahman Gazel <02210224066@ogr.inonu.edu.tr>
2026-04-15 15:22:16 +0300

    Adjust thread sleep durations for ChatServer and ChatClient initialization

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -3,20 +3,25 @@ package Main;
 public class Main {
 
     public static void main(String[] args) {
-        ChatServer.main(args);
+
 
         new Thread(() -> {
-            try { Thread.sleep(800); } catch (InterruptedException ignored) {}
+            try { Thread.sleep(1000); } catch (InterruptedException ignored) {}
+            ChatServer.main(args);
+        }, "server").start();
+
+        new Thread(() -> {
+            try { Thread.sleep(2000); } catch (InterruptedException ignored) {}
             ChatClient.main(args);
         }, "client-1").start();
 
         new Thread(() -> {
-            try { Thread.sleep(1600); } catch (InterruptedException ignored) {}
+            try { Thread.sleep(3000); } catch (InterruptedException ignored) {}
             ChatClient.main(args);
         }, "client-2").start();
 
         new Thread(() -> {
-            try { Thread.sleep(2400); } catch (InterruptedException ignored) {}
+            try { Thread.sleep(4000); } catch (InterruptedException ignored) {}
             ChatClient.main(args);
         }, "client-3").start();
     }
